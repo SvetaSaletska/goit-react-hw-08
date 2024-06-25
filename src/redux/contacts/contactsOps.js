7cd71905189cc8429761936bc1cebdaf7a6dfdba
SvetaSaletska <svitlanagalevich@gmail.com>
2024-06-25 21:20:06 +0300

    update

--- src/redux/contacts/contactsOps.js
+++ src/redux/contacts/contactsOps.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://6679933118a459f639508b99.mockapi.io";
+axios.defaults.baseURL = "https://connections-api.goit.global";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
